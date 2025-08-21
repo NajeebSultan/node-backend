767f1ae3a48909c0fe34a14a10427f9b9a13dd64
NajeebSultan <najeebsultan070@gmail.com>
2025-08-21 23:35:35 +0500

    feat: add routes for root, apple, and orange paths in Express server

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -6,8 +6,22 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+app.get("/", (req, res) => {
+  res.send("you contacted root path");
+});
 
-app.use((req , res) => {
-  console.log("Request received");
+app.get("/apple", (req, res) => {
+  res.send("you contacted apple path");
+});
 
-}) ;
+app.get("/orange", (req, res) => {
+  res.send("you contacted orange path");
+});
+
+
+
+// app.use((req , res) => {
+//   console.log("Request received");
+//   res.send("Hello, World!");
+
+// }) ;
