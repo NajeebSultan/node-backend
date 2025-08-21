6bf753caab8fe0f037a1ad98f701b0fd6960cb23
NajeebSultan <najeebsultan070@gmail.com>
2025-08-21 15:07:31 +0500

    fix: update figlet text from "Hello World!!" to "Najeeb!!"

--- FigletDir/index.js
+++ FigletDir/index.js
@@ -1,6 +1,6 @@
 const figlet = require("figlet");
 
-figlet("Hello World!!", function (err, data) {
+figlet("Najeeb!!", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
