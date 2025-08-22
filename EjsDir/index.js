8105b094b9f469d0ac55cbc7daf822b2c31223fb
NajeebSultan <najeebsultan070@gmail.com>
2025-08-22 17:17:19 +0500

    Make  ejs file for rendering

--- EjsDir/index.js
+++ EjsDir/index.js
@@ -4,7 +4,9 @@ const app = express();
 let port = 8080;
 
 app.set('view engine', 'ejs');
-
+app.get('/', (req, res) => {
+  res.send('hi i am a root path');
+}); 
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
