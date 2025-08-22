974b5ca20bdb824e4393f0c23143b9eb86caf58f
NajeebSultan <najeebsultan070@gmail.com>
2025-08-22 17:13:48 +0500

    Start our server

---
+++ EjsDir/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+let port = 8080;
+
+app.set('view engine', 'ejs');
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
