6beae730b42fb401c3d937f2b171b42f15d68604
NajeebSultan <najeebsultan070@gmail.com>
2025-08-22 18:00:14 +0500

    Add view and path

--- EjsDir/index.js
+++ EjsDir/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 let port = 8080;
 
 app.set('view engine', 'ejs');
+app.set("views",Path.join(__dirname , "/views")) ; 
+
+
 app.get('/', (req, res) => {
   res.render("home.ejs");
 });     
+
+app.get("hello", (req, res) => { 
+  res.send("hello from express");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
