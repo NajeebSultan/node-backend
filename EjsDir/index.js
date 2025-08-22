cade3076c9eebd88a331f80ee5969d346503fb27
NajeebSultan <najeebsultan070@gmail.com>
2025-08-22 18:21:00 +0500

    Make  a roll dice using randomnumber

--- EjsDir/index.js
+++ EjsDir/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 let port = 8080;
 
 app.set('view engine', 'ejs');
-app.set("views",Path.join(__dirname , "/views")) ; 
+app.set("views",path.join(__dirname , "/views")) ; 
 
 
 app.get('/', (req, res) => {
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
 
 app.get("hello", (req, res) => { 
   res.send("hello from express");
+});
+
+app.get("/rolldice", (req, res) => {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs" ,  {randomNumber}  );
 });
 
 app.listen(port, () => {
